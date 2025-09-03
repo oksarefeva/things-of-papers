4825d692e563f10ccb14ff6277dbb1014fd2756b
Oksana Arefeva <oksanaarefy@MacBook-Pro-Oksana.local>
2025-09-03 16:34:03 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+i18next
+    .use(i18nextHttpBackend)
+    .init({
+        lng: 'cs',
+        backend: {
+            loadPath: '/locales/{{lng}}/translation.json'
+        }
+    })
+    .then(() => {
+        updateContent();
+    });
+
+function updateContent() {
+    document.querySelectorAll('[data-i18n]').forEach(el => {
+        const key = el.getAttribute('data-i18n');
+        el.innerHTML = i18next.t(key);
+        console.log('jazyk:', i18next.t(key))
+    });
+}
+
+document.querySelectorAll('.dropdown-item')
+    .forEach(item => {
+        item.addEventListener('click', e => {
+            i18next.changeLanguage(e.currentTarget.dataset.lang).then(updateContent);
+        });
+    });
